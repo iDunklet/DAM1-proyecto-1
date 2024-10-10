e35bb29877ed12981849e6f7dfe506f57d457fb1
iDunklet <152725396+iDunklet@users.noreply.github.com>
2024-10-10 11:35:25 +0200

    dale um revisada que queda poco

--- Main.java
+++ Main.java
@@ -8,7 +8,8 @@ import java.util.Map;
 public class Main{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //variables
+
+        //?variables
         int energy = randomizer();
         int fatigue = randomizer();
         int hygiene = randomizer();
@@ -18,7 +19,7 @@ public class Main{
         int opcion = -1;
 
 
-        //start of the game
+        //?start of the game
         System.out.println("\nWelcome to the Virtual Pet!");
         System.out.println("\nWhat pet would you like to adopt? (cat, dog)");
         String pet_result = scanner.nextLine();
@@ -47,7 +48,7 @@ public class Main{
 
         //game loop
         try {
-            while (opcion != 0){
+            do {
                 System.out.println("\n******************************");
                 System.out.printf("| %-28s |\n", "Bienvenido a nuestro menu!");
                 System.out.println("------------------------------");
@@ -56,15 +57,16 @@ public class Main{
                 System.out.printf("| %-2d. %-25s |\n", 3, "Eat");
 
                 if (animal == 'c') {
-                    System.out.printf("| %-2d. %-25s |\n", 4, "Shower");
-                } else {
                     System.out.printf("| %-2d. %-25s |\n", 4, "Self-cleaning");
+                } else {
+                    System.out.printf("| %-2d. %-25s |\n", 4, "Shower");
                 }
-
+                System.out.printf("| %-2d. %-25s |\n", 5, "Pet status");
                 System.out.printf("| %-2d. %-25s |\n", 0, "Exit application");
                 System.out.println("******************************\n");
                 System.out.print("Choose an option(number): ");
                 opcion = scanner.nextInt();
+                scanner.nextLine();
                 switch (opcion) {
                     case 0:
                         System.out.println("Exiting the application...");
@@ -84,10 +86,10 @@ public class Main{
 
                     case 2:
                         if (animal == 'c'){
-                            System.out.println("You played with your"+pet_result+"!");
+                            System.out.println("You played with your "+pet_result+"!");
                             sleep=sleep-20;
                         } else {
-                            System.out.println("You played with your"+pet_result+"!");
+                            System.out.println("You played with your "+pet_result+"!");
                             sleep=sleep-15;
                         }
                         fatigue=fatigue-15;
@@ -116,12 +118,24 @@ public class Main{
                             hygiene=hygiene-20;
                         }
                         break;
+                    case 5:
+                        System.out.println("\n| Your pet's status:\n");
+                        System.out.printf("| Energy: "+energy+"\n");
+                        System.out.printf("| Fatigue: "+fatigue+"\n");
+                        System.out.printf("| Hygiene: "+hygiene+"\n");
+                        System.out.printf("| Weight: "+weight+"\n");
+                        System.out.printf("| Sleep: "+sleep+"\n");
+                        System.out.println("Press any key to come back to original menu...");
+                        scanner.nextLine();
+                        break;
+                    case 6:
+
                     default:
                         System.out.println("Invalid option, please try again!");
                         break;
                 }
-                check_life(energy, fatigue, hygiene, weight);
-            }
+                check_life(energy, fatigue, hygiene, weight, sleep, scanner);
+            }while (opcion!= 0);
         }catch (InputMismatchException e){
             System.out.println("Invalid input, please enter a number!\n");
             scanner.nextLine();
@@ -136,7 +150,7 @@ public class Main{
         return randomValue;
     }
 
-    public static int check_life(int energy, int fatigue, int hygiene, int weight){
+    public static int check_life(int energy, int fatigue, int hygiene, int weight,int sleep, Scanner scanner){
 
         int[] life_status = {energy, fatigue, hygiene, weight};
         String[] Slife_status = {"energy", "fatigue", "hygiene", "weight"};
@@ -147,11 +161,15 @@ public class Main{
             if (life_check > 100){
                 System.out.println("\nYour pet died");
                 System.out.println("by high: "+Slife_status[i]);
+                scanner.close();
                 System.exit(0);
+
             } else if (life_check <= 0) {
                 System.out.println("\nyour pet died");
                 System.out.println("by low: "+Slife_status[i]);
+                scanner.close();
                 System.exit(0);
+
             }
         }
         return 1;
